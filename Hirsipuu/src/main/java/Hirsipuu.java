f814365f9736535ad31b4d7f98c33575a3302e41
mixu12 <71936620+mixu12@users.noreply.github.com>
2021-06-15 21:18:07 +0300

    Add files via upload

--- Hirsipuu/src/main/java/Hirsipuu.java
+++ Hirsipuu/src/main/java/Hirsipuu.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.geometry.Insets;
@@ -6,35 +5,185 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Hirsipuu extends Application{
+	public int arvaukset;
+	public int vaarat;
+	
+	public Hirsipuu() {
+		this.arvaukset = 0;
+		this.vaarat = 0;
+	}
     
         @Override
         public void start(Stage ikkuna){
-        BorderPane asettelu = new BorderPane();
-        
-        VBox painikkeet = new VBox();
-        
-        Button pelaa = new Button("Pelaa");
-        Button lopeta = new Button("Lopeta");
-        
-        painikkeet.getChildren().addAll(pelaa, lopeta);
-        
-        asettelu.setCenter(painikkeet);
-        
-        Scene nakyma = new Scene(asettelu, 640, 480);
-        
-        ikkuna.setScene(nakyma);
-        ikkuna.show();
-    }
-    
+        	// Luodaan tarpeelliset oliot
+            
+            Sanalista sanalista = new Sanalista();            
+            
+        	sanalista.lataaSanat();
+            
+            //asettelu
+	        BorderPane asettelu = new BorderPane();
+	        
+	        HBox painikkeet = new HBox();
+	        
+	        Button pelaa = new Button("Pelaa");
+	        Button lopeta = new Button("Lopeta");
+	        Button sanat = new Button("Näytä sanat");
+	        Button lisaaSana = new Button("Lisää sana");
+	        
+	        painikkeet.getChildren().addAll(pelaa, sanat, lisaaSana, lopeta);
+	        
+	        Label tekstikentta = new Label("");
+
+	        
+	        //Painikkeiden komennot
+	        sanat.setOnMouseClicked((event) ->{
+	        	asettelu.setBottom(null);
+	        	asettelu.setLeft(null);
+	        	asettelu.setCenter(sanalista.getLista());
+	        	asettelu.setRight(sanalista.poistaRiviListViewista());
+	        });
+	        
+	        	        
+	        lisaaSana.setOnMouseClicked((event) ->{
+	        	asettelu.setBottom(null);
+	        	asettelu.setLeft(null);
+	        	asettelu.setCenter(sanalista.lisaaSanaNakyma());
+	        	asettelu.setRight(tekstikentta);
+	        	
+	        });
+	        
+	        lopeta.setOnMouseClicked((event) ->{
+	        	sanalista.tallenna();
+	        	ikkuna.close();
+	        });
+	        //Tästä alkaa peli
+	        pelaa.setOnMouseClicked((event) ->{
+	        	this.arvaukset = 0;
+	        	this.vaarat = 0;
+	        	
+	        	//vasempaan reunaan tulevat muotoilut
+	        	VBox vasenReuna = new VBox();
+	        	vasenReuna.setAlignment(Pos.CENTER);
+	        	Label arvaustenMaara = new Label();
+	        	Label virheet = new Label();
+	        	Label arvatutKirjaimet = new Label();
+	        	
+	        	vasenReuna.getChildren().addAll(arvaustenMaara, virheet, arvatutKirjaimet);
+	        	asettelu.setLeft(vasenReuna);
+	        	arvaustenMaara.setText("Arvauksia " + this.arvaukset);
+	        	virheet.setText("Virheitä " + this.vaarat);
+	        	
+                //lukee sanan ja pilkkoo sen
+                Sana sana = new Sana(sanalista.getSana());
+                String[] osat = sana.etsittySana().split("");
+                
+                //apumuuttuja, jonka avulla piilotetaan sana näkyvistä arvauksissa
+                String[] arvattu = sana.etsittySana().split("");
+                
+                StringBuilder piilotettu = new StringBuilder();
+                for(int i = 0; i < sana.etsittySana().length();i++){
+                    arvattu[i] = " _ ";
+                    piilotettu.append(arvattu[i]);
+                }
+                
+    	        Label arvattavaSana = new Label(piilotettu.toString());
+                asettelu.setCenter(arvattavaSana);
+                
+                //kirjaimen syöttö
+                GridPane asetteluPelissa = new GridPane();
+                
+
+                Label kirjain = new Label("Kirjain");
+                TextField sanakentta = new TextField();
+                Label tyhjaTekstikentta = new Label("");
+                
+                asettelu.setRight(tyhjaTekstikentta);
+
+                asetteluPelissa.setAlignment(Pos.CENTER);
+                asetteluPelissa.setVgap(10);
+                asetteluPelissa.setHgap(10);
+                asetteluPelissa.setPadding(new Insets(10, 10, 10, 10));
+
+                Button annaKirjain = new Button("Syötä kirjain");
+
+                asetteluPelissa.add(kirjain, 0, 0);
+                asetteluPelissa.add(sanakentta, 0, 1);
+                asetteluPelissa.add(annaKirjain, 0, 3);
+
+                //Kirjaimen syöttöpainike
+                
+                annaKirjain.setOnMouseClicked(ae -> {
+                	
+                    String kirjainKentasta = sanakentta.getText();
+                    arvaukset += 1; 
+                    
+                    if(arvaukset == 1) {
+                    	arvatutKirjaimet.setText(kirjainKentasta);
+                    } else {
+                    	arvatutKirjaimet.setText(arvatutKirjaimet.getText() + ", " + kirjainKentasta);
+                    }
+                                                                 
+                    if(sana.etsittySana().contains(kirjainKentasta) && !(kirjainKentasta.isBlank()) ){
+
+                    	//Käy läpi, sisältääkö sana kirjaimen
+                        StringBuilder nayta = new StringBuilder();
+                        for(int i = 0; i < sana.etsittySana().length();i++){
+                            if(osat[i].contains(kirjainKentasta)){
+                                    arvattu[i] = osat[i];
+                                    
+                            }
+                            //Tämä tulee, jos sanan kaikki kirjaimet ovat oikein
+                            nayta.append(arvattu[i]);
+                            	if(nayta.toString().equals(sana.etsittySana())) {
+                            		Label voitto = new Label("Voitit pelin!");
+                            		asetteluPelissa.add(voitto, 0, 1);
+                            	}
+
+                        }  
+
+                        arvattavaSana.setText(nayta.toString());                        
+                        asettelu.setLeft(vasenReuna);
+                        asettelu.setCenter(arvattavaSana);
+                        
+                    } else {                      
+                        	this.vaarat += 1;
+                        	if(this.vaarat == 7) {
+	                        	Label havisit = new Label("Hävisit pelin!");
+	                        	asettelu.setCenter(havisit);
+	                        	asettelu.setBottom(null);
+                        	}                    	
+                    	
+                    }
+                    
+                    arvaustenMaara.setText("Arvauksia " + this.arvaukset);
+                    virheet.setText("Virheitä " + this.vaarat);
+                    
+                    sanakentta.clear();
+                });
+                asettelu.setBottom(asetteluPelissa);
+                
+	        });
+	        
+	        //loppuasettelu
+	        
+	        asettelu.setTop(painikkeet);
+	        
+	        Scene nakyma = new Scene(asettelu, 640, 480);
+	        
+	        ikkuna.setScene(nakyma);
+	        ikkuna.show();
+	    }
+	    
     public static void main(String[] args){
         launch(Hirsipuu.class);
-
     } 
 }
--- Hirsipuu/src/main/java/Sanalista.java
+++ Hirsipuu/src/main/java/Sanalista.java
@@ -1,21 +1,89 @@
 import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class Sanalista {
-    private ArrayList<String> sanalista;
+    private List<String> sanalista;
+    private ListView<String> listaus;
     
     public Sanalista(){
         this.sanalista = new ArrayList<>();
+        this.listaus = new ListView();
+    }
+    
+    
+    public ListView<String> getLista() {
+    	return this.listaus;
+    }
+    
+    public Parent poistaRiviListViewista() {
+        GridPane asettelu = new GridPane();
+
+
+        asettelu.setAlignment(Pos.CENTER);
+        asettelu.setVgap(10);
+        asettelu.setHgap(10);
+        asettelu.setPadding(new Insets(10, 10, 10, 10));
+
+        Button poistanappi = new Button("Poista valittu sana");
+
+        asettelu.add(poistanappi, 0, 0);
+       
+        poistanappi.setOnMouseClicked((event) ->{
+        	int poistettava = this.listaus.getSelectionModel().getSelectedIndex();
+        	poistaSana(poistettava);
+        	this.listaus.getItems().removeAll(this.listaus.getSelectionModel().getSelectedItems());
+        });
+
+        return asettelu; 	
     }
     
     public void lisaaSana(String lisattava){           
         if(!(sanalista.contains(lisattava))){
             sanalista.add(lisattava);
+            listaus.getItems().add(lisattava);
         }            
+    }
+    
+    public Parent lisaaSanaNakyma() {
+        GridPane asettelu = new GridPane();
+
+        Label sanaohje = new Label("Sana");
+        TextField sanakentta = new TextField();
+
+        asettelu.setAlignment(Pos.CENTER);
+        asettelu.setVgap(10);
+        asettelu.setHgap(10);
+        asettelu.setPadding(new Insets(10, 10, 10, 10));
+
+        Button lisaanappi = new Button("Lisää sana");
+
+        asettelu.add(sanaohje, 0, 0);
+        asettelu.add(sanakentta, 0, 1);
+        asettelu.add(lisaanappi, 0, 4);
+
+        lisaanappi.setOnMouseClicked((event) -> {
+            String sana = sanakentta.getText();
+
+            lisaaSana(sana);
+
+            sanakentta.clear();
+        });
+        
+        return asettelu; 	
     }
        
     public String getSana(){
@@ -60,7 +128,7 @@ public class Sanalista {
     }
     
     public void lataaSanat(){
-        String tiedostonimi = "\"C:\\\\Users\\\\kimjo\\\\Documents\\\\NetBeansProjects\\\\mavenproject3\\\\Miikan\\\\sanat.txt\"";
+        String tiedostonimi = "C:\\Users\\kimjo\\Documents\\NetBeansProjects\\mavenproject3\\Miikan\\sanat.txt";
         try (Scanner tiedostonLukija = new Scanner(Paths.get(tiedostonimi))) {
             
             while (tiedostonLukija.hasNextLine()) {
@@ -73,7 +141,7 @@ public class Sanalista {
     }
     
     public void tallenna(){
-        String tiedostonimi = "\"C:\\\\Users\\\\kimjo\\\\Documents\\\\NetBeansProjects\\\\mavenproject3\\\\Miikan\\\\sanat.txt\"";
+        String tiedostonimi = "C:\\Users\\kimjo\\Documents\\NetBeansProjects\\mavenproject3\\Miikan\\sanat.txt";
         try(PrintWriter kirjoittaja = new PrintWriter(tiedostonimi)){
             for(int i = 0; i < this.sanalista.size(); i++){
                     kirjoittaja.println(this.sanalista.get(i));                   
@@ -89,7 +157,8 @@ public class Sanalista {
     }
     
     public void poistaSana(int sananNumero){
-        this.sanalista.remove(sananNumero - 1);
+        this.sanalista.remove(sananNumero);
         
     }
+    
 }
